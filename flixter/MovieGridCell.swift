a0ae242c686aea7d9aca39f9dcc94fcb3b1fb727
Amy Salley <alsalley@icloud.com>
2020-09-26 19:41:22 -0600

    Grid screen second draft

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Amys code on 9/26/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
